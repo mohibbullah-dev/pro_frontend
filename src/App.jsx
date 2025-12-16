41127d093e5afb4912147b9cab419bf06f9ba723
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-12-16 21:57:29 +0200

    modified: app.jsx Auth.jsx & add restuarant images

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Orders from "./pages/Orders";
@@ -6,21 +11,30 @@ import Header from "./components/shared/Header";
 import Tables from "./pages/Tables";
 import Menu from "./pages/Menu";
 
-function App() {
+function Layout() {
+  const { pathname } = useLocation();
+
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/table" element={<Tables />} />
-          <Route path="/menu" element={<Menu />} />
-        </Routes>
-      </Router>
+      {pathname !== "/auth" && <Header />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/table" element={<Tables />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
     </>
   );
 }
+
+const App = () => {
+  return (
+    <Router>
+      <Layout />
+    </Router>
+  );
+};
 
 export default App;
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import restaurantImage from "../assets/images/restaurantImage.jpg";
+import logo_2 from "../assets/images/logo_2.png";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div className="flex min-h-screen w-full">
+      {/* Left Section  */}
+      <div className="wl/2 relative flex items-center justify-center bg-cover">
+        {/* BG Image  */}
+        <img
+          className="w-full h-full object-cover"
+          src={restaurantImage}
+          alt="restanrant Image"
+        />
+        {/* blacke overley  */}
+        <div className="absolute inset-0 bg-black opacity-80"></div>
+
+        <blockquote className="absolute bottom-10 px-8 mb-10 text-2xl italic text-white">
+          "Server customer the best food wieth prompt and friendly sevice in a
+          welcoming atmosphere, and they'all kiip coming back."
+          <br />
+          <span className="block mt-4 text-yellow-400">Founder of Restro</span>
+        </blockquote>
+      </div>
+
+      {/* Right Section  */}
+
+      <div className="w-1/2 min-h-screen bg-[#1a1a1a] p-10">
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src={logo_2}
+            alt="logo"
+            className="h-14 w-14 border-2 rounded-full p-1"
+          />
+          <h1 className="text-lg font-semibold text-[#f5f5f5] tracking-wide">
+            Restro
+          </h1>
+        </div>
+        <h2 className="text-4xl text-center mt-10 font-semibold text-yellow-400 mg-10">
+          Employee Registration
+        </h2>
+
+        <div className="flex justify-center gap-1 mt-6">
+          <p className="text-sm text-[#ababab]">Already have an account? </p>
+          <a className="text-yellow-400 font-semibold hover:underline" href="#">
+            Sign in
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
